198e35df2ef53d6135993c4e46a435f6499ef00b
Lavyk Soares <lavyk_@hotmail.com>
2017-11-28 19:48:24 -0300

    Criando o metodo insert [insertAux]

--- src/implementacao4/BST.java
+++ src/implementacao4/BST.java
@@ -10,20 +10,60 @@ package implementacao4;
  * @author usuario
  */
 class BST implements BST_IF {
-    
-    
+
+    private BSTNode root;
 
     public BST() {
+        this.root = new BSTNode();
     }
 
     @Override
     public void insert(int element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.root.isEmpty()) {
+            this.root.setData(element);
+            this.root.setParent(null);
+        }
+    }
+
+    private void insertAux(BSTNode node, int element) {
+        if (element < (int) node.getData()) {
+            if (node.getLeft() == null) {
+                node.setLeft(new BSTNode(element));
+                node.getLeft().setParent(node);
+            } else {
+                insertAux(node.getLeft(), element);
+            }
+        } else if (element > node.key) {
+            if (node.right == null) {
+                node.right = new Node(element);
+                node.right.parent = node;
+            } else {
+                insertHelper(node.right, element);
+            }
+        } else {
+            return;
+        }
     }
 
     @Override
     public int search(int element) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            BSTNode aux = root;
+
+            if (!aux.isEmpty()) {
+                while ((int) aux.getData() != (element)) {
+                    if ((int) aux.getData() > element) {
+                        aux = aux.getLeft();
+                    } else {
+                        aux = aux.getRight();
+                    }
+                }
+            }
+            return (int) aux.getData();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        return -1;
     }
 
     @Override
@@ -45,5 +85,5 @@ class BST implements BST_IF {
     public boolean isComplete() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
--- src/implementacao4/BSTNode.java
+++ src/implementacao4/BSTNode.java
@@ -5,10 +5,71 @@
  */
 package implementacao4;
 
+import java.util.Objects;
+
 /**
  *
  * @author usuario
  */
 public class BSTNode {
+
+    private Object data;
+    private BSTNode left;
+    private BSTNode right;
+    private BSTNode parent;
     
+    public BSTNode (){
+        
+    }
+    
+    public BSTNode(int element) {
+        this.data = element;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public BSTNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BSTNode left) {
+        this.left = left;
+    }
+
+    public BSTNode getRight() {
+        return right;
+    }
+
+    public void setRight(BSTNode right) {
+        this.right = right;
+    }
+
+    public BSTNode getParent() {
+        return parent;
+    }
+
+    public void setParent(BSTNode parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "BSTNode{" + "data=" + data + ", left=" + left + ", right=" + right + ", parent=" + parent + '}';
+    }
+
+    public boolean isEmpty() {
+        return this.data == null;
+    }
 }
